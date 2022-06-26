137b0afaa708de6622c213b63cc571b87241abc4
hafitzrizki <hafitzrizki@gmail.com>
2022-06-26 23:37:08 +0700

    menyelesaikan tantangan Intro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='card'>
+            <img src={props.data.photo} alt={props.data.name}/>
+            <div className='wrap-text'>
+                <h2>{props.data.name}</h2>
+                <p>{props.data.phone}</p>
+                <p>{props.data.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <section>
+                <h2>Call a Friend</h2>
+                <p>your friendly contact app</p>
+            </section>
+            <hr/>
+        </>
     )
 }
 
